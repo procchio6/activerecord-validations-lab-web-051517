71dec2e4bb8e631fa14ade93360fc5e8e4255061
Pat Rocchio <procchio6@gmail.com>
2017-06-12 10:23:27 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+  validate :clickbaity
+
+  def clickbaity
+    if !title.nil?
+      if title.match(/(Won't Believe)|(Secret)|(Top\s\d+)|(Guess)/).nil?
+        errors.add(:clickbaity, 'Title must be clickbaity!')
+        false
+      end
+    else
+      false
+    end
+  end
 end
